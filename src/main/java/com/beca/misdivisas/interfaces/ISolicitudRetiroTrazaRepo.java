adf7fb805471995643cf05622fbd13831e175b25
Jose Ferreira <jose.ferreira@bancoexterior.com>
2021-11-20 21:18:27 -0400

    Ajuste para obviar el estatus post-procesada al consultar por ultimo estatus

--- src/main/java/com/beca/misdivisas/interfaces/ISolicitudRetiroTrazaRepo.java
+++ src/main/java/com/beca/misdivisas/interfaces/ISolicitudRetiroTrazaRepo.java
@@ -19,7 +19,7 @@ public interface ISolicitudRetiroTrazaRepo extends JpaRepository<SolicitudRetiro
 	public List<SolicitudRetiroTraza> findByIdSolicitud(Integer solicitudIds);
 
 	@Query("SELECT t FROM SolicitudRetiroTraza t where t.idSolicitudRetiroTraza in (SELECT	max(s.idSolicitudRetiroTraza) "
-			+ "FROM	SolicitudRetiroTraza s WHERE s.idSolicitud in (?1) and s.fecha >= ?2 and s.fecha <= ?3 group by s.idSolicitud)")
+			+ "FROM	SolicitudRetiroTraza s WHERE s.idSolicitud in (?1) and s.fecha >= ?2 and s.fecha <= ?3 and s.idEstatusSolicitud != 8 group by s.idSolicitud)")
 	public List<SolicitudRetiroTraza> findByIdSolicitudInAndFecha(List<Integer> solicitudIds, Date fechaInicio,
 			Date fechaFin);
 
